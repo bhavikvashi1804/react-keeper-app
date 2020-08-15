64a647df825902924eb0e8689e04aef2657b34a4
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-08-15 15:47:21 +0530

    ES6 Arrow Function

--- src/components/emoji/App.jsx
+++ src/components/emoji/App.jsx
@@ -3,24 +3,22 @@ import EmojiOne from "./EmojiOne";
 import emojipedia from "./emojipedia";
 import "./style.css";
 
-function createEmoji(emoji1) {
-  return (
-    <EmojiOne
-      key={emoji1.id}
-      emoji={emoji1.emoji}
-      name={emoji1.name}
-      meaning={emoji1.meaning}
-    />
-  );
-}
-
 function App() {
   return (
     <div>
       <h1>
         <span>emojipedia</span>
       </h1>
-      <dl className="dictionary">{emojipedia.map(createEmoji)}</dl>
+      <dl className="dictionary">
+        {emojipedia.map(emoji1 => (
+          <EmojiOne
+            key={emoji1.id}
+            emoji={emoji1.emoji}
+            name={emoji1.name}
+            meaning={emoji1.meaning}
+          />
+        ))}
+      </dl>
     </div>
   );
 }
