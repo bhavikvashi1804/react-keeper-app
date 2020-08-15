bb594a040e9ab3adf14a99eae9db2b0c3b7e3d64
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-08-15 14:40:09 +0530

    JS object map to React Component

--- src/components/Contact/App.jsx
+++ src/components/Contact/App.jsx
@@ -6,30 +6,28 @@ import Avatar from './Avatar';
 
 import "./style.css";
 
+function createCard(contact){
+  return (
+    <Card 
+      key={contact.id}
+      //key must require for list
+      //key is not prop
+      //you cannot access this key in component
+      name={contact.name}
+      image={contact.imgURL}
+      mobile={contact.phone}
+      email={contact.email}
+    />
+  );
+}
+
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
       <Avatar image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT3s55O_no6FMNB63sweRv8SW6eweXI2ZCBCg&usqp=CAU" />
-      <Card
-        name={contacts[0].name}
-        image={contacts[0].imgURL}
-        mobile={contacts[0].phone}
-        email={contacts[0].email}
-      />
-      <Card
-        name={contacts[1].name}
-        image={contacts[1].imgURL}
-        mobile={contacts[1].phone}
-        email={contacts[1].email}
-      />
-      <Card
-        name={contacts[2].name}
-        image={contacts[2].imgURL}
-        mobile={contacts[2].phone}
-        email={contacts[2].email}
-      />
+      {contacts.map(createCard)}
     </div>
   );
 }
