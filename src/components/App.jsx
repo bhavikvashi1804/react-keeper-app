80f843269058361001cb0291f38769e5920f3ee1
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-08-15 15:59:29 +0530

    ES6 map and arrow function in Keeper App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,11 +4,17 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 
+import notes from './notes';
+
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(note=><Note
+        key={note.id}
+        title={note.title}
+        content={note.content}
+      />)}
       <Footer />
     </div>
   );
