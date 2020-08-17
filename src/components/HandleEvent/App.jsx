31463bb72dd2d9a332e04ec9a2154037fbf6cea7
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-08-17 16:03:04 +0530

    onMouseOver and onMouseOut

--- src/components/HandleEvent/App.jsx
+++ src/components/HandleEvent/App.jsx
@@ -3,17 +3,33 @@ import "./style.css";
 
 function App() {
   const [headingText, setHeadingText] = useState("Hello");
+  const [isMouseOver, setMouseOver] = useState(false);
 
   function handleClick() {
     console.log("Button is clicked");
     setHeadingText("Data uploaded");
   }
 
+  function onMouseOverFunction() {
+    setMouseOver(true);
+  }
+
+  function onMouseOutFunction() {
+    setMouseOver(false);
+  }
+
   return (
     <div className="container">
       <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button onClick={handleClick}>Submit</button>
+      <button
+        style={{ backgroundColor: isMouseOver?"black":"white" }}
+        onClick={handleClick}
+        onMouseOver={onMouseOverFunction}
+        onMouseOut={onMouseOutFunction}
+      >
+        Submit
+      </button>
     </div>
   );
 }
