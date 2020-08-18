c6ad39aeb56614c875d9cf7b9263139af31c7373
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-08-18 12:06:46 +0530

    Task done of Handle the user data from forms

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './components/HandleEvent/App';
+import App from './components/ContactMe/App';
 
 ReactDOM.render(
   <App />,
