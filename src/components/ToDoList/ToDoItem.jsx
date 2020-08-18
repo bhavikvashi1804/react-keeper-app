7f977b5bc3b9aff34c5c9126878038f27de39d9a
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-08-18 14:17:21 +0530

    Click to delete start project

--- src/components/ToDoList/ToDoItem.jsx
+++ src/components/ToDoList/ToDoItem.jsx
@@ -1,14 +1,9 @@
 import React,{useState} from "react";
 
 function ToDoItem(props) {
-  const [taskDone, setTaskDone] = useState(false);
-  function changeDoneStatus() {
-    setTaskDone((prevValue) => {
-      return !prevValue;
-    });
-  }
-  return (<div onClick={changeDoneStatus}>
-    <li style={{textDecoration:taskDone?"line-through":"none"}}>{props.toDoItemText}</li>
+  
+  return (<div >
+    <li >{props.toDoItemText}</li>
   </div>);
 }
 
